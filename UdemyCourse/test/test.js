ce083d4ab83ae1fd7e3047da03b665d7c9231b2a
Murat Ozcan <muratkerem@gmail.com>
2018-06-09 08:31:23 -0500

    sinon

--- UdemyCourse/test/test.js
+++ UdemyCourse/test/test.js
@@ -7,14 +7,49 @@ var chaiAsPromised = require("chai-as-promised");
 var sinon = require('sinon');
 
 describe ('Add tests', function () {
-  it ('should return 3 when called with 1 and 2', function() {
-    var result = Add(1,2);
-    var expectedResult = 4;
-    assert.equal(expectedResult, result);
+  var result = Add(1,2);
+  var expectedResult = 3;
+  describe('Add tests with ASSERT STYLE', function() {
+    it ('test addition ', function() {
+      var invalidResult = 99;
+      assert.equal(expectedResult, result);
+    });
+    it ('test addition ASSERT NOT', function() {
+      var invalidResult = 99;
+      assert.notEqual(expectedResult, invalidResult);
+    });
   });
-  it ('should return 8 when called with 5 and 3', function() {
-    var result = Add(5,3);
-    var expectedResult = 8;
-    assert.equal(expectedResult, result);
+  describe('Add tests with SHOULD STYLE', function() {
+    it ('test addition ', function() {
+      // var result = Add(10, 11);
+      // var expectedResult = 20;
+      result.should.equal(expectedResult);
+    });
+    it ('test additon with SHOULD NOT ', function() {
+      // var result = Add(10, 11);
+      var invalidResult = 20;
+      result.should.not.equal(invalidResult);
+    });
   });
+  describe('Add tests with EXPECT STYLE', function() {
+    it ('test addition ', function() {
+      // var result = Add(10, 10);
+      // var expectedResult = 20;
+      expect(result).to.equal(expectedResult);
+    });
+    it ('test addition EXPECT NOT ', function() {
+      var invalidResult = 50;
+      expect(result).not.equal(invalidResult);
+    });
+  });
+});
+describe ('Spy Tests', function () {
+  var numberOne = 1;
+  var numberTwo = 2;
+  it ('should log result of add', function() {
+    var logSpy = sinon.spy();
+    Add(numberOne, numberTwo, logSpy);
+  });
+
+
 });
